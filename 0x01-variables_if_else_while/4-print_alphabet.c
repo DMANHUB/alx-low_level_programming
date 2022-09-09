950cc84ec92ae613c5fd268b7facf6a099ae78ae
DMANHUB <iamaechi58@gmail.com>
2022-09-09 16:29:04 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers of base 10 starting from
+ * All your code should be in the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+int n;
+while (n < 10)
+{
+printf("%d", n);
+n += 1;
+}
+printf("\n");
+return (0);
+}
+Thank you dear
+Did it end with 5?
+i think so
+Wow
+Ok
+So how did you finish it
+am not done
+Ok
+It's not sha
