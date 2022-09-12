057aa3e2ffc84b2047a21638d4aafb3af1fa3caa
DMANHUB <iamaechi58@gmail.com>
2022-09-12 16:22:33 -0700

    cmt

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * * main - program that prints the alphabet in lowercase, and then in uppercase,
- *   * You can only use the putchar
- *    * Return: 0;
+ * main - program that prints the alphabet in lowercase, and then in uppercase,
+ * You can only use the putcha
+ * Return: 0;
  */
 
 int main(void)
 {
 	int lw = 'a';
 	int up = 'A';
-	while (lw <= 'z')
+	while (lw < = 'z'\n')
 	{
 		putchar(lw);
 		lw += 1;
