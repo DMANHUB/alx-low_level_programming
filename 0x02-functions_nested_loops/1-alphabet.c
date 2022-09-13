422bfbf3415cfca95d19e16c75e2419464396514
DMANHUB <iamaechi58@gmail.com>
2022-09-13 16:39:35 -0700

    commit2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * print_alphabet - a function that prints the alphabet, in lowercase
  *
  * Return: Always 0 (Succes)
@@ -11,12 +10,12 @@ void print_alphabet(void)
 
 {
 	int n;
+	
 	for (n = 'a'; n <= 'z'; n++)
 {
 	_putchar(n);
 }
 	_putchar('\n');
 	
-	return 0;
 }
 
