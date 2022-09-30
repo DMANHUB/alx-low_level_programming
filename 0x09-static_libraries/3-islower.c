2b9e94e545982804bdf907000cf5b7a5628d258b
DMANHUB <iamaechi58@gmail.com>
2022-09-30 15:22:24 -0700

    0x09-static_libraries 0task

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * check - the code
+ *
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
